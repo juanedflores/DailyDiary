ff79e2ac4131e8eef5767bbb43ddbabea4dfba89
Juan Eduardo <juanedflores@gmail.com>
2024-08-04 02:22:05 -0500

    changed to markdown

--- rplugin/python3/diary_template.py
+++ rplugin/python3/diary_template.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
 import pynvim
 import sys
 import datetime
 import calendar
+
 
 @pynvim.plugin
 class DiaryTemplate(object):
@@ -13,10 +13,14 @@ class DiaryTemplate(object):
         global diaryPath
         self.nvim = nvim
         diaryPath = self.nvim.command_output("echom g:vimwikidiaryPath")
+        self.nvim.out_write('started plugin')
+        self.nvim.out_write("\n")
 
-    @pynvim.autocmd('BufNewFile', pattern=diaryPath+'/*.wiki', eval=None, sync=False)
+    @pynvim.autocmd('BufNewFile', pattern='~/*.wiki', eval=None, sync=False)
     def test(self):
         global diaryPath
+        self.nvim.command_output("echom g:vimwikidiaryPath")
+        self.nvim.command_output("\n")
         buf = self.nvim.current.buffer
 
         dow = calendar.day_name[datetime.date.today().weekday()]
@@ -26,13 +30,17 @@ class DiaryTemplate(object):
 
         today_date = (datetime.date.today())
 
-        line1 = "== " + dow + ", " + m + " " + d + " =="
+        line1 = "# " + dow + ", " + m + " " + d
         line2 = ""
-        line3 = "=== Goals for Today | due:({}) ===".format(today_date)
+        line3 = "## Due Soon | due:({})".format(today_date)
         line4 = ""
-        line5 = "=== Issues Encountered ==="
+        line5 = "## Goals for Today | status:pending".format(today_date)
         line6 = ""
-        line7 = "=== Notes ==="
+        line7 = "## Issues Encountered"
+        line8 = ""
+        line9 = "## Notes"
+        line10 = ""
+        line11 = '![[{}-canvas.canvas]]'.format(today_date)
 
         buf[0] = line1
         buf.append(line2, index=-1)
@@ -41,8 +49,52 @@ class DiaryTemplate(object):
         buf.append(line5, index=-1)
         buf.append(line6, index=-1)
         buf.append(line7, index=-1)
-        self.nvim.out_write('loading diary template!')
+        buf.append(line8, index=-1)
+        buf.append(line9, index=-1)
+        buf.append(line10, index=-1)
+        buf.append(line11, index=-1)
+        # self.nvim.out_write('loaded diary template!')
+        # self.nvim.out_write("\n")
 
+    @pynvim.autocmd('BufNewFile', pattern=diaryPath+'/*.md', eval=None, sync=False)
+    def testmd(self):
+        global diaryPath
+        self.nvim.command_output("echom g:vimwikidiaryPath")
+        self.nvim.command_output("\n")
+        buf = self.nvim.current.buffer
+
+        dow = calendar.day_name[datetime.date.today().weekday()]
+        m = datetime.datetime.now().strftime("%b")
+        day = datetime.datetime.today().day
+        d = ordinal(day)
+
+        today_date = (datetime.date.today())
+
+        line1 = "# " + dow + ", " + m + " " + d
+        line2 = ""
+        line3 = "## Due Soon | due:({})".format(today_date)
+        line4 = ""
+        line5 = "## Goals for Today | status:pending".format(today_date)
+        line6 = ""
+        line7 = "## Issues Encountered"
+        line8 = ""
+        line9 = "## Notes"
+        line10 = ""
+        line11 = '![[{}-canvas.canvas]]'.format(today_date)
+
+        buf[0] = line1
+        buf.append(line2, index=-1)
+        buf.append(line3, index=-1)
+        buf.append(line4, index=-1)
+        buf.append(line5, index=-1)
+        buf.append(line6, index=-1)
+        buf.append(line7, index=-1)
+        buf.append(line8, index=-1)
+        buf.append(line9, index=-1)
+        buf.append(line10, index=-1)
+        buf.append(line11, index=-1)
+        # self.nvim.out_write('loading diary template!')
+        # self.nvim.out_write("\n")
 
 
 def ordinal(num):
